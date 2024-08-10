a0d6050e5d25ad6db1c90079989c87916e4c30e2
AShwin <ashwin.thanga@gmail.com>
2024-08-10 13:20:23 +0530

    fixed cors issue

--- SERVER/index.js
+++ SERVER/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const app = express();
 const { Server } = require("socket.io");
 
-app.use(cors);
+app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
